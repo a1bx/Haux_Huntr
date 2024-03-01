8fb646d2c2bb88aad107278eb20f332e0c2f47dc
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-03-01 21:40:23 +0300

    update

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class GenerateQRCode extends StatefulWidget {
-  const GenerateQRCode({super.key});
+  const GenerateQRCode({Key? key}) : super(key: key);
 
   @override
   State<GenerateQRCode> createState() => _GenerateQRCodeState();
@@ -10,32 +10,47 @@ class GenerateQRCode extends StatefulWidget {
 
 class _GenerateQRCodeState extends State<GenerateQRCode> {
   TextEditingController urlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text('Generate QR Code'),),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if(urlController.text.isNotEmpty)
-                QrImageView(data:urlController.text,size:200,),
-              SizedBox(height:10,),
-              Container(
-                padding: EdgeInsets.only(left:10,right:10),
-                child: TextField(
-                  controller: urlController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your data',
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-                    labelText: 'Enter your text'
+      appBar: AppBar(
+        title: Text('Generate QR Code'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.grey, Colors.blue],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (urlController.text.isNotEmpty)
+                  QrImageView(data: urlController.text, size: 200),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: TextField(
+                    controller: urlController,
+                    decoration: InputDecoration(
+                        hintText: 'Enter your data',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        labelText: 'Enter your text'),
                   ),
                 ),
-              ),
-              SizedBox(height:10,),
-              ElevatedButton(onPressed: (){}, child: Text('Generate QR Code')),
-            ],
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Generate QR Code'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
