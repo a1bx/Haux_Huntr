9a237af640f5b034b56bd96ee550774dd33721f3
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-04-18 17:58:59 +0300

    update

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -26,57 +26,62 @@ class _GenerateQRCodeState extends State<GenerateQRCode> {
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(color: Colors.grey),
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+        ),
         child: Center(
           child: SingleChildScrollView(
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-            child: Container(
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/img.png', // Replace with your image path
-                    height: 150, // Adjust the height as needed
-                  ),
-                  SizedBox(height: 20),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      color: Colors.grey[200],
-                      child: TextField(
-
-                        controller: urlController,
-                        decoration: InputDecoration(
-                          hintText: 'Enter your text',
-                          border: InputBorder.none,
+            child: Card(
+              elevation: 4, // Adjust elevation as needed
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/img.png', // Replace with your image path
+                      height: 150, // Adjust the height as needed
+                    ),
+                    SizedBox(height: 20),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        color: Colors.grey[100],
+                        child: TextField(
+                          controller: urlController,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your text',
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        qrData = urlController.text;
-                      });
-                    },
-                    child: Text(
-                      'Generate QR Code',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal,
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          qrData = urlController.text;
+                        });
+                      },
+                      child: Text(
+                        'Generate QR Code',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  if (qrData.isNotEmpty)
-                    QrImageView(
-                      data: qrData,
-                      size: 300,
-                    ),
-                ],
+                    SizedBox(height: 20),
+                    if (qrData.isNotEmpty)
+                      QrImageView(
+                        data: qrData,
+                        size: 300,
+                      ),
+                  ],
+                ),
               ),
             ),
           ),
