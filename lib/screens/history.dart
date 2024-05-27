4587777f33c0215e7282b018a6ca0157ad5407bb
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-05-27 18:25:17 +0300

    update

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -32,7 +32,10 @@ class QRHistoryPage extends StatefulWidget {
   _QRHistoryPageState createState() => _QRHistoryPageState();
 }
 
-class _QRHistoryPageState extends State<QRHistoryPage> {
+class _QRHistoryPageState extends State<QRHistoryPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
   final List<String> scanHistory = [
     'https://itunes.com',
     'https://itunes.com',
@@ -42,6 +45,31 @@ class _QRHistoryPageState extends State<QRHistoryPage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0.0, -1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    // Start the animation as soon as the page is loaded
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -49,31 +77,38 @@ class _QRHistoryPageState extends State<QRHistoryPage> {
       ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Scan',
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
+          SlideTransition(
+            position: _offsetAnimation,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              color: Colors.grey[200],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Scan',
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Create',
+                      style: TextStyle(fontSize: 18, color: Colors.blue),
+                    ),
+                  ),
+                ],
               ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Create',
-                  style: TextStyle(fontSize: 18, color: Colors.blue),
-                ),
-              ),
-            ],
+            ),
           ),
           Expanded(
             child: ListView.builder(
               itemCount: scanHistory.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  leading: Icon(Icons.qr_code),
+                  leading: Icon(Icons.qr_code, color: Colors.teal),
                   title: Text(scanHistory[index]),
                   subtitle: Text('16 Dec 2022, 9:30 pm'),
                   trailing: IconButton(
