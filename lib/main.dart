3192c440a056ced0eb2df59eff84cdfaa5ff5979
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-05-27 15:06:30 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,22 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:haux_huntr/generate_qr_code.dart';
-import 'package:haux_huntr/scan_qr_code.dart';
-import 'package:haux_huntr/splash_screen.dart';
-import 'package:haux_huntr/history.dart';
+import 'package:haux_huntr/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Haux Huntr',
       debugShowCheckedModeBanner: false,
+      title: 'Haux Huntr',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: SplashScreen(),
-    );
-  }
-}
-
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Haux Huntr',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.teal,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // SizedBox(height: 0),
-              Image.asset(
-                'assets/images/img.png',
-                height: 150,
-                width: 150,
-              ),
-              SizedBox(height: 80),
-              SizedBox(height: 40),
-              Text(
-                'Pick any option to begin',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 80),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => ScanQRCode()),
-                    );
-                  });
-                },
-                child: Text(
-                    'Scan Qr code',
-                  style: TextStyle(
-                    color: Colors.teal,
-                  ),
-                ),
-              ),
-              SizedBox(height: 48),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => GenerateQRCode()),
-                    );
-                  });
-                },
-                child: Text(
-                    'Generate QR Code',
-                  style: TextStyle(
-                    // fontSize: 20.0,
-                    // fontWeight: FontWeight.bold,
-                    color: Colors.teal,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.teal[700],
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.grey[200],
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('History'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => HistoryPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Log out'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/screens/onboarding-screen.dart
@@ -0,0 +1,52 @@
+// onboarding_screen.dart
+import 'package:flutter/material.dart';
+import 'package:product_id_blockchain/screens/home_screen.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  final List<PageViewModel> pages = [
+    PageViewModel(
+      title: 'Welcome to Product ID Blockchain',
+      body: 'Verify the authenticity of products using blockchain technology.',
+      image: Center(
+        child: Image.asset('assets/images/onboarding1.png'),
+      ),
+    ),
+    PageViewModel(
+      title: 'Scan Product QR Code',
+      body: 'Simply scan the QR code on the product to verify its authenticity.',
+      image: Center(
+        child: Image.asset('assets/images/onboarding2.png'),
+      ),
+    ),
+    PageViewModel(
+      title: 'Secure and Transparent',
+      body: 'Blockchain ensures transparency and security in product verification.',
+      image: Center(
+        child: Image.asset('assets/images/onboarding3.png'),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: pages,
+      onDone: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      },
+      showSkipButton: true,
+      skip: Text('Skip'),
+      next: Icon(Icons.arrow_forward),
+      done: Text('Get Started'),
+      dotsDecorator: DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Colors.grey,
+        activeColor: Colors.blue,
+      ),
+    );
+  }
+}
