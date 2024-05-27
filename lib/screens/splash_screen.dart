222b439626c4d31dd0c221a88a3dded91954aacb
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-05-27 15:13:06 +0300

    update

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,59 +1,28 @@
+// splash_screen.dart
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:haux_huntr/main.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:haux_huntr/screens/onboarding-screen.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(Duration(seconds: 3),(){
-     Navigator.of(context).pushReplacement(MaterialPageRoute(
-         builder: (_)=> const Homepage(),
-     ));
-    }
-    );
-  }
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: SystemUiOverlay.values);
-    super.dispose();
-  }
-
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        color: Colors.teal,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(
-              Icons.qr_code_2_rounded,
-              size: 80,
-              color: Colors.white,
+    return AnimatedSplashScreen(
+      duration: 5000,
+      splash: Column(
+        children: [
+          // Image.asset('assets/images/blue_logo.png'),
+          Text(
+            'Haux Huntr',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.teal
             ),
-            SizedBox(height: 20),
-            Text(
-              'Haux Huntr',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white
-              )
-            )
-          ],
-        ),
+          ),
+        ],
       ),
+      nextScreen: OnboardingScreen(),
+      splashTransition: SplashTransition.fadeTransition,
     );
   }
 }
