af8b1e9af72d8e6de1df3ddafd626adf9f37de21
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-06-12 14:14:34 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-// main.dart
 import 'package:flutter/material.dart';
+// import 'package:haux_huntr/screens/scan_qr_code.dart';
 import 'package:haux_huntr/screens/splash_screen.dart';
+// import 'package:haux_huntr/web3dart/web3_service.dart';
+// import 'qr_scanner.dart';
+// import 'package:flutter_web3/flutter_web3.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
